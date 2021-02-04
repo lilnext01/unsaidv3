f7f447a4477c570dddd24ad99ab3e9449248154d
lilnext01 <74841546+lilnext01@users.noreply.github.com>
2021-02-04 08:00:16 -0300

    Add files via upload

---
+++ src/unsaidmenu.js
@@ -0,0 +1,12 @@
+const unsaidmenu = (prefix, pushname) => {
+    return `❍ *Comandos do Unsaid*
+    
+    ║➩ ❍ ${prefix}setprefix
+    ║➩ ❍ ${prefix}block
+    ║➩ ❍ ${prefix}tm
+    ║➩ ❍ ${prefix}tmctt
+    ║➩ ❍ ${prefix}clearall`
+
+}
+
+exports.unsaidmenu = unsaidmenu
